c168b46c7505b8d329276238997a850845582bac
sakshamkhare18 <sakshamkhare563@gmail.com>
2024-07-09 11:38:22 +0530

    added index.html

---
+++ first code/script.js
@@ -0,0 +1,5 @@
+const wrapper2 = document.queryselector('.wrapper2');
+const btnpopup = document.queryselector('.btnlogin-popup');
+
+
+btnpopup.addeventlistener('click',()=>{wrapper2.classlist .add('Active-popup');});
